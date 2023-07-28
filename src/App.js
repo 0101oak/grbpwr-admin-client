02f947b2b173c8091f4a43eb1724d9e40348b944
0101oak <115372729+0101oak@users.noreply.github.com>
2023-07-28 13:10:44 +0300

    feat: add links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// import { useState } from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Navbar from './components/navbar';
+import Add from './pages/add/add';
+import Product from './pages/product/product';
 
-function App() {
+export default function Admin() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Add />}></Route>
+          <Route path='product' element={<Product />}></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
-
-export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='links'>
+        <Link to="/product"> product </Link>
+        <Link to="/"> add </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
